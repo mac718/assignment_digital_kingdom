1f50b910fa226a8d0c451c09f0fc1110589c4e03
mac718 <mac718@gmail.com>
2018-10-26 17:12:40 -0400

    Finish castle adding capability

--- routes/index.js
+++ routes/index.js
@@ -11,17 +11,24 @@ router.get('/', (req, res) => {
     });
 })
 
-router.get('/kingdoms/:kingdom', (req, res) => {
-  console.log(req.params.kingdom);
+router.get('/kingdoms/:kingdom/:id', (req, res) => {
   kingdom.getCastleInfo(req.params.kingdom)
     .then(result => {
       //result = JSON.parse(result);
       console.log(result)
-      res.render('kingdoms/show', {result: result, kingdom: req.params.kingdom});
+      res.render('kingdoms/show', {result: result, kingdom: req.params.kingdom, id: req.params.id});
     })    
 })
 
-router.post('/', (req, res) => {
+router.get('/castles/:castle', (req, res) => {
+  kingdom.getLiegeInfo(req.params.castle)
+    .then(result => {
+      //result = JSON.parse(result);
+      res.render('castles/show', {result: result, castle: req.params.castle});
+    })    
+})
+
+router.post('/kingdom', (req, res) => {
   let name = req.body.name;
   let king = req.body.king;
   let queen = req.body.queen;
@@ -30,6 +37,17 @@ router.post('/', (req, res) => {
       res.redirect('/');
     });
   //console.log(req.body.name);
+})
+
+router.post('/castle', (req, res) => {
+  let name = req.body.castleName;
+  let kingdomId = req.body.kingdomId;
+  let kingdomName = req.body.kingdomName;
+  console.log(kingdomId);
+  kingdom.addCastle(name, kingdomId)
+    .then(() => {
+      res.redirect('back');
+    });
 })
 
 module.exports = router;
